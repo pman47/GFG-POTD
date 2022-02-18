5e99b55d466fb51d9e6fd02a5f3af84471cf1a78
Manish <manishprajapatim416@gmail.com>
2022-02-18 12:12:42 +0530

    POTD 18-02-22

---
+++ Rearrange Geek and his Classmates/code.java
@@ -0,0 +1,10 @@
+class Solution 
+{ 
+    void prank(long[] a, int n)  
+    {
+        for(int i=0;i<n;i++){
+            a[i] = a[i] + (a[(int)a[i]] % n) * n ;
+        }
+        for(int i=0;i<n;i++) a[i]=a[i]/n;
+    }
+} 
