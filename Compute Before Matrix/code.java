5749eb2a13ab5142055636ed56d9966b6a79c2c2
Manish <manishprajapatim416@gmail.com>
2022-03-06 19:12:43 +0530

    POTD 06-03-22

---
+++ Compute Before Matrix/code.java
@@ -0,0 +1,15 @@
+class Solution{
+    public int[][] computeBeforeMatrix(int N, int M,int[][] after ){
+        int a[][]=new int [N][M];
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                if(i==0 && j==0) a[0][0]=after[0][0];
+                if(i==0 && j!=0) a[i][j]=after[i][j]-after[i][j-1];
+                if(j==0 && i!=0) a[i][j]=after[i][j]-after[i-1][j];
+                if(i!=0 && j!=0)
+                a[i][j]=after[i][j]+after[i-1][j-1]-after[i][j-1]-after[i-1][j];
+            }
+        }
+        return a;
+    }
+}
